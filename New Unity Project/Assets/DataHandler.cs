613bfdd9fa4db9eafb07bdbf4d16386c37600662
FLAMINGxFURY <fury.etain.5@gmail.com>
2021-09-28 21:27:07 -0400

    I can now actually open a file

--- New Unity Project/Assets/DataHandler.cs
+++ New Unity Project/Assets/DataHandler.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InstructionSetSimulation.Core;
 
 public class DataHandler : MonoBehaviour
 {
-    CPU cpu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        data.cpu = new CPU();
     }
 
     // Update is called once per frame
--- New Unity Project/Assets/OFButtonHandler.cs
+++ New Unity Project/Assets/OFButtonHandler.cs
@@ -7,6 +7,27 @@ using UnityEngine.UI;
 public class OFButtonHandler : MonoBehaviour
 {
     public void createOFD() {
-		EditorUtility.OpenFilePanel("File", "../", "*");
+		data.cpu.Rd.fileStr = EditorUtility.OpenFilePanel("File", "../", "*");
+		data.cpu.Rd.OpenFile();
+
+		//Display Hex for Program
+		List<string> dispProgMem = new List<string>();
+
+		foreach (ushort x in data.cpu.Rd.proMem) {
+			string add = x.ToString("X4"); //4 digits of hex
+			add = add.Insert(2, " ");
+			dispProgMem.Add(add);
+		}
+		
+		string progDisplay = "";
+		
+		foreach (string s in dispProgMem) {
+			progDisplay += (s + " ");
+		}		
+
+		Text binaryText = GameObject.Find("Binary Text").GetComponent<Text>();
+
+		binaryText.text = progDisplay;
+
 	}
 }
